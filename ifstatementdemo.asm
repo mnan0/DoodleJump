3d0c9518fb8871692c2fa384cef236a8d6d63bf3
mnan0 <50372901+mnan0@users.noreply.github.com>
2020-11-29 17:57:18 -0500

    finished if statement learning

--- ifstatementdemo.asm
+++ ifstatementdemo.asm
@@ -2,5 +2,10 @@
 	#displayAddress: .word 0x10008000
 .text
 	add $t0,$zero,$zero	#$t0 = 0
-	addi $t1,$zero,0	#$t1 = 0	
-	
+	addi $t1,$zero,0	#$t1 = 0
+	addi $t2,$zero,100	#$t2 = 100
+START:	beq $t1, $t2, END
+	addi $t1,$t1,1
+	j START		
+END:	li $v0, 10
+	syscall
