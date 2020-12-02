fa57a1eb2b94dd2d438d23db8e3476028a32a205
mnan0 <50372901+mnan0@users.noreply.github.com>
2020-12-02 14:49:37 -0500

    nested function test

--- workpad.s
+++ workpad.s
@@ -4,8 +4,22 @@
 	fry: .word 1
 	displayAddress: .word 0x10008000
 .text
-	addi $t0,$zero,-1
-	
+	# NESTED FUNCTION TEST
+	addi $a0,$a0,2
+	addi $a1,$a1,1
+	jal multiply_four
+	add $t0,$zero,$v0
+	j EXIT
+multiply_four: 
+	sll $v0,$a0,2
+	addi $sp,$sp,-4
+	sw $ra,0($sp)
+	jal add_function
+	lw $ra,0($sp)
+	addi $sp,$sp,4
+	jr $ra
+add_function	:add $v0,$v0,$a1
+	jr $ra
 	
 EXIT: 	li $v0 10
 	syscall
